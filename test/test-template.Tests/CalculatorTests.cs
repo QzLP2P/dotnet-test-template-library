b9cad29345bac085d5f324f6310280689e5810cb
Thomas GICQUEL <m.gicquel.thomas@gmail.com>
2025-12-11 10:55:51 +0100

    tests

--- test/test-template.Tests/CalculatorTests.cs
+++ test/test-template.Tests/CalculatorTests.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Thomas. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using test-template;
+using TestsTemplate;
 using Xunit;
 
 public class CalculatorTests
